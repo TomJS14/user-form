cbbb9e89a5d626dc7b21432f29c4adf93b2de230
TomJS14 <133436558+TomJS14@users.noreply.github.com>
2023-07-09 11:11:02 +0100

    set up

--- index.js
+++ index.js
@@ -5,30 +5,50 @@ const countryInput = document.querySelector("#country");
 const postcodeInput = document.querySelector("#postcode");
 const passwordInput = document.querySelector("#password");
 const confirmPasswordInput = document.querySelector("#confirmPassword");
-
 const submit = document.querySelector(".submit");
 
 //error elements
-const emailError = document.querySelector(".email-error");
-const userNameError = document.querySelector(".username-error");
-const postCodeError = document.querySelector(".postcode-error");
-const passwordError = document.querySelector(".password-error");
-const confirmError = document.querySelector(".confirm-error");
+const emailError = document.querySelector(".email");
+const userNameError = document.querySelector(".username");
+const postCodeError = document.querySelector(".postcode");
+const passwordError = document.querySelector(".password");
+const confirmError = document.querySelector(".confirm");
+
+//Submit disabled by default
+submit.disabled = true;
+
+//check all inputs valid before enabling submit
+
+function checkSubmit() {
+  if (!confirmPasswordInput.value == null);
+  {
+    if (
+      emailInput.validity.valid &&
+      userNameInput.validity.valid &&
+      postcodeInput.validity.valid &&
+      passwordInput.validity.valid &&
+      confirmPasswordInput.validity.valid
+    ) {
+      submit.disabled = false;
+    }
+  }
+}
 
 /* Error handling */
 function showError() {
   if (!emailInput.validity.valid) {
     emailError.textContent = "Please enter a valid email";
   }
-  emailError.className = "error active";
   if (!userNameInput.validity.valid) {
     userNameError.textContent = "Too few characters";
   }
-  userNameError.className = "error active";
   if (!postcodeInput.validity.valid) {
     postCodeError.textContent = "Enter a valid postcode";
   }
-  postCodeError.className = "error active";
+  if (!passwordInput.validity.valid) {
+    passwordError.textContent =
+      "Please enter a password longer than 5 characters";
+  }
 }
 
 /* Event listeners */
@@ -36,16 +56,17 @@ emailInput.addEventListener("keyup", (e) => {
   if (emailInput.validity.valid) {
     emailError.textContent = "";
     emailError.className = "error";
+    checkSubmit();
   } else {
     showError();
   }
 });
 
 userNameInput.addEventListener("keyup", (e) => {
-  console.log(userNameInput.validity.tooShort);
   if (!userNameInput.validity.tooShort) {
     userNameError.textContent = "";
     userNameError.className = "error";
+    checkSubmit();
   } else {
     showError();
   }
@@ -56,24 +77,35 @@ postcodeInput.addEventListener("keyup", (e) => {
   if (postcodeInput.validity.valid) {
     postCodeError.textContent = "";
     postCodeError.className = "error";
-  } else if (i > 4) {
+    checkSubmit();
+  } else {
+    submit.disabled = true;
     showError();
   }
 });
 
-passwordInput.addEventListener("keyup", passwordCheck);
+passwordInput.addEventListener("keyup", (e) => {
+  if (passwordInput.validity.valid) {
+    passwordError.textContent = "";
+    passwordError.className = "error";
+    checkSubmit();
+  } else {
+    showError();
+  }
+});
+
 confirmPasswordInput.addEventListener("keyup", passwordCheck);
+passwordInput.addEventListener("keyup", passwordCheck);
 
 function passwordCheck() {
   if (confirmPasswordInput.value === passwordInput.value) {
-    confirmPasswordInput.setAttribute("style", "background-color: green");
+    confirmPasswordInput.setAttribute("style", "border: 2px solid green");
     confirmError.textContent = "";
-    submit.disabled = false;
+    checkSubmit();
   } else {
-    confirmPasswordInput.setAttribute("style", "background-color: red");
+    confirmPasswordInput.setAttribute("style", "border: 2px solid red");
     confirmError.setAttribute("style", "color: white");
     confirmError.textContent = "Passwords do not match";
-
     submit.disabled = true;
   }
 }
